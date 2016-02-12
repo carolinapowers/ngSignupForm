0fb1cb8ebbb450a2feac29589ccdf4c76d48b0eb
Carolina Powers <carolinapoloni@gmail.com>
2016-02-11 22:42:04 -0500

    including angular via cdn for gh-pages

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -1,7 +1,7 @@
-(function() {
-  'use strict';
+(function () {
+    'use strict';
 
-  angular
-    .module('ngSignupForm', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ui.router', 'ui.bootstrap', 'toastr']);
+    angular
+        .module('ngSignupForm', ['ui.router']);
 
 })();
